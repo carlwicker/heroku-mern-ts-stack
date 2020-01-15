9be1f722a0553d925ae9766bb69379dfe7046436
Carl Wicker <carlwicker@gmail.com>
2020-01-15 13:45:47 +0000

    concurrently

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Heroku MERN Stack (TypeScript Version)
 
 https://heroku-mern-ts-stack.herokuapp.com/
+
+Includes HEROKU_POSTBUILD scripts for easy deployment from GitHub directly to Heroku.
+
+npm run dev
+
+This will run the concurrently package to enable ExpressJS and ReactJS at the same time.
+
+ReactJS Dev: http://localhost:3000
+ExpressJS Dev: http://localhost:5000/api
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Express Port
+const port = process.env.PORT || 5000;
 
 // Config Environmental Variables
 require("dotenv").config();
 
-// Test End Point
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Express Body Parser
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Mongoose Connection|
+const db = process.env.MONGODB_URI;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Static React Build Folder
+app.use(express.static(path.join(__dirname, "client/build/")));
+
+// Express API Build
+app.get("/api", (req, res) => res.send("Express End Point"));
+
+// Get React Build
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "client", "build", "index.html"));
 });
 
 app.listen(port, () => console.log("Listening on Port: " + port));
