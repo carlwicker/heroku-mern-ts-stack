c835a19293dedc51558e9c5025060fc9ce8a39f7
Carl Wicker <carlwicker@gmail.com>
2020-01-15 16:41:29 +0000

    server notes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // Express Port
 const port = process.env.PORT || 5000;
 
-// Config Environmental Variables
+// !!! CONFIGURE .env FILE IN ROOT FOR ENVIRONMENTAL VARIABLES !!!
 require("dotenv").config();
 
 // Express Body Parser
@@ -24,10 +24,10 @@ app.use(cors());
 // Static React Build Folder
 app.use(express.static(path.join(__dirname, "client/build/")));
 
-// Express API Build
+// Direct to Express API Endpoint
 app.get("/api", (req, res) => res.json({ test: "success" }));
 
-// Get React Build
+// Direct to React Build
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "client", "build", "index.html"));
 });
